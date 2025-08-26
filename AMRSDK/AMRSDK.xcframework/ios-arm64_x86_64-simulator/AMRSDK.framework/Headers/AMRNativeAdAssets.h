4fcd62806fb9b31ce3188cf6874f3f081677dac1
Mehmet Karagöz <eng.mkaragoz@gmail.com>
2025-08-26 14:19:32 +0300

    1.5.75

---
+++ AMRSDK/AMRSDK.xcframework/ios-arm64_x86_64-simulator/AMRSDK.framework/Headers/AMRNativeAdAssets.h
@@ -0,0 +1,19 @@
+//
+//  AMRNativeAdAssets.h
+//  AMRSDK
+//
+//  Created by Mehmet Karagöz on 26.08.2025.
+//  Copyright © 2025 Admost. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AMRNativeAdAssets : NSObject
+@property NSString *title;
+@property NSString *desc;
+@property NSString *ctaText;
+@end
+
+NS_ASSUME_NONNULL_END
